33fd04019087e8cd6df06315b7e13b042ab362e1
Andréa Franco <dedefac96@gmail.com>
2024-08-14 18:45:16 +0200

    Clean

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
