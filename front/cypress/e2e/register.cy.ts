d131034813231d2e7a8cb6244d61875291ea1f23
Andréa Franco <dedefac96@gmail.com>
2024-08-18 21:02:12 +0200

    reformatted login and register e2e tests

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,16 +1,18 @@
 describe('Register spec', () => {
-    it('Register successfull', () => {
-      cy.visit('/register')
-  
-      cy.intercept('POST', '/api/auth/register', {
-        body: {}
-      })
+  it('Register successfully', () => {
+    cy.visit('/register');
 
-      cy.get('input[formControlName=firstName]').type("Andrea")
-      cy.get('input[formControlName=lastName]').type("Franco")
-      cy.get('input[formControlName=email]').type("yoga@studio.com")
-      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-  
-      cy.url().should('include', '/login')
-    })
+    cy.intercept('POST', '/api/auth/register', {
+      body: {},
+    });
+
+    cy.get('input[formControlName=firstName]').type('Andrea');
+    cy.get('input[formControlName=lastName]').type('Franco');
+    cy.get('input[formControlName=email]').type('yoga@studio.com');
+    cy.get('input[formControlName=password]').type(
+      `${'test!1234'}{enter}{enter}`
+    );
+
+    cy.url().should('include', '/login');
   });
+});
