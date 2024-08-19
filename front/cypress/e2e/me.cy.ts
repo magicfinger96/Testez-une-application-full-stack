b0827974e115e8bb1e89eeb1614aa8dc5fe4a37e
Andréa Franco <dedefac96@gmail.com>
2024-08-19 17:37:46 +0200

    e2e test: delete user

--- front/cypress/e2e/me.cy.ts
+++ front/cypress/e2e/me.cy.ts
@@ -20,5 +20,24 @@ describe('Me spec', () => {
 
     cy.get('span[routerLink=me]').click();
     cy.url().should('include', '/me');
+
+    cy.contains('Name: jane DOE').should('be.visible');
+    cy.contains('Email: jane.doe@live.fr').should('be.visible');
+    cy.contains('Create at: August 19, 2024').should('be.visible');
+    cy.contains('Last update: August 19, 2024').should('be.visible');
+  });
+
+  it('should delete user when hitting delete button', () => {
+    cy.intercept('GET', '/api/user/1', {
+      fixture: 'user.json',
+    });
+
+    cy.get('span[routerLink=me]').click();
+
+    cy.intercept('DELETE', '/api/user/1', {});
+
+    cy.get('button[mat-raised-button]').click();
+
+    cy.url().should('equal', Cypress.config('baseUrl'));
   });
 });
